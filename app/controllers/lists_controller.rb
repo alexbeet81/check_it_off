ff399a734eaf3a850bc469a2623633cb01396493
Alex Pritchard <alexbeet1234@gmail.com>
2021-08-16 16:55:57 +0800

    created index html

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,19 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def create
+
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 end
