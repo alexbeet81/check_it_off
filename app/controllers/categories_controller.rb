f660fa11f3234fcc986fe1050a8ff75ed84c1fb1
Kristoffer Duthu <kristoffer.duthu@gmail.com>
2021-08-16 18:20:47 +0800

    added controller actions to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,34 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, except: [:create]
+  before_action :set_list
+  def create
+    @category = Category.new
+    @category.list = @list
+
+    if @category.save
+      redirect_to list_path(@list)
+    else
+      render "list/show"
+    end
+
+  end
+
+  def destroy
+    @category.destroy
+
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def category_params; end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
