8a7614a03a4c4fda3f320d69ac267315723bcc1b
Alex Pritchard <alexbeet1234@gmail.com>
2021-08-17 22:56:29 +0800

    build a test front end for shopping - stopped at toggle list selected

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,11 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true
 
-   def no_name
+  def no_name
     self.item_name = "uhhhh cat food????" if self.item_name.empty?
+  end
+
+  def toggle_item_selected
+    self.item_selected ? self.item_selected = false : self.item_selected = true
   end
 end
