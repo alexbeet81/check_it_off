2c8675f2d191a91bcccca36964ebd4a69f5929be
Alex Pritchard <alexbeet1234@gmail.com>
2021-09-04 13:19:20 +0800

    added blank div to show to get the list id

--- app/javascript/packs/init_create_category.js
+++ app/javascript/packs/init_create_category.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 const inItCreateCategory = () => {
+  // find the id of the current list.
+  const getListId = document.getElementById('data-id');
+  const id = getListId.dataset.id;
+
   const meatFish = document.getElementById('meat_fish');
 
   const categoryModal = document.getElementById('categoryModal');
@@ -8,21 +12,25 @@ const inItCreateCategory = () => {
   if (meatFish != null) {
     meatFish.addEventListener('click', (e) => {
       // AIP call to create a new category.
-      // find the list id
+
       // create an object {catname: ,list_id: }that is then passed into a funtion
      
     // AIP call to create a new category.
-     axios.post('http://localhost:3000/api/v1/lists/2/categories', {
-        category_name: "Meat & Fish",
-        list_id: 2
-      })
+    //  axios.post(`http://localhost:3000/api/v1/lists/${id}/categories`, {
+    //     category_name: "Meat & Fish",
+    //     list_id: id
+    //   })
 
+      apiCall('Meat & Bla!', id)
       categoryModal.style.display = 'none';
     })
   }
 
-  const apiCall = (data) => {
-    //
+  const apiCall = (name, id) => {
+    axios.post(`http://localhost:3000/api/v1/lists/${id}/categories`, {
+      category_name: name,
+      list_id: id
+    })
   };
 };
 
