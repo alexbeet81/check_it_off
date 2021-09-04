ea607c0cf7e9e955fe0f44769a40e7b0406c01e0
Alex Pritchard <alexbeet1234@gmail.com>
2021-09-04 08:10:12 +0800

    moved all divs form categoryselect into show - this didn't work yesterday, but today it did because things....

--- app/javascript/packs/init_create_category.js
+++ app/javascript/packs/init_create_category.js
@@ -1,6 +1,11 @@
 const inItCreateCategory = () => {
-  // const meatFish = document.
+  const meatFish = document.getElementById('meat_fish');
 
+  console.log(meatFish);
+
+  meatFish.addEventListener('click', (e) => {
+    console.log(e)
+  })
 };
 
 export default inItCreateCategory;
