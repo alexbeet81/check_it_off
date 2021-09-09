39591bf2c95cb9a9b56db771fae9d0ac270d29e3
Alex Pritchard <alexbeet1234@gmail.com>
2021-09-09 10:48:22 +0800

    created a new variable in lists_controller that finds the last category id - passed through via a dataset and used to delete a newly created category in the dom

--- app/javascript/packs/init_create_category.js
+++ app/javascript/packs/init_create_category.js
@@ -4,11 +4,19 @@ const inItCreateCategory = () => {
 
   const categoryCards = document.querySelectorAll('.category_card');
 
+  const categoryContainer = document.querySelector('.category-container');
   // find the id of the current list.
   const getListId = document.getElementById('data-id');
 
   // get the category modal and set display to none after api call
   const categoryModal = document.getElementById('categoryModal');
+  
+  //get the id of each category - find the last id
+  const getItemAdd = document.querySelectorAll('.item-add');
+
+  getItemAdd.forEach(e => {
+    const id = e.dataset.id;
+  })
 
   // categoryCards.forEach( categoryCard => {
   //   categoryCard.addEventListener('click', e => {
@@ -19,19 +27,30 @@ const inItCreateCategory = () => {
   // conditional to stop error when not on list#show page
   if (categoryCards.length > 1) {
     const id = getListId.dataset.id;
+    // find the last category id
+    const lastId = getListId.dataset.lastid;
+    console.log(lastId);
 
     categoryCards[0].addEventListener('click', (e) => {
       apiCall('Meat & Fish', '#d8315b', '#ffecf0', id)
       categoryModal.style.display = 'none';
+      
+      // call the newCategoryDiv funcation and save it to div.
+      const div = newCategoryDiv('Meat & Fish', '#d8315b', lastId);
+      console.log(div);
+      console.log(id);
+      categoryContainer.insertAdjacentHTML("beforeend", div);
     })
 
     categoryCards[1].addEventListener('click', (e) => {
       apiCall('Fruit & Veg', '#50723c', '#e1ffd0', id)
+      console.log(id)
       categoryModal.style.display = 'none';
     })
 
     categoryCards[2].addEventListener('click', (e) => {
       apiCall('Snacks', '#f2a540', '#fff0dc', id)
+      console.log(id)
       categoryModal.style.display = 'none';
     })
 
@@ -95,6 +114,25 @@ const inItCreateCategory = () => {
       categoryModal.style.display = 'none';
     })
   }
+
+  // function that creates a new div that is called and then added
+  // to the category-container
+  const newCategoryDiv = (categoryName, color, lastId) => {
+    // api call to find the last id
+    const categoryId = parseInt(lastId) + 1;
+    const div = `
+      <div class="category-item" style="background-color: ${color};">
+        <div class="category-title">
+          <h3>${categoryName}</h3>
+        </div>
+        <div class="icon-group-cat">
+          
+          <a data-confirm="Are you quite sure?" class="fas fa-trash" rel="nofollow" data-method="delete" href="/categories/${categoryId}"></a>
+        </div>
+      </div>
+    `
+    return div;
+  };
 
 
   const apiCall = (name, colorOne, colorTwo, id) => {
